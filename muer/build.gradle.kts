97e76fe1abe7427bdfbe5d01cbddbba4b0423dd7
Tamas Sipos <tamas.sipos@zooshgroup.com>
2023-04-17 23:20:45 +0200

    setup project

---
+++ muer/build.gradle.kts
@@ -0,0 +1,48 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "3.0.5"
+	id("io.spring.dependency-management") version "1.1.0"
+	kotlin("jvm") version "1.7.22"
+	kotlin("plugin.spring") version "1.7.22"
+	kotlin("plugin.jpa") version "1.7.22"
+}
+
+group = "sch.aut"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudAzureVersion"] = "5.0.0"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.azure.spring:spring-cloud-azure-starter-active-directory")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("com.microsoft.sqlserver:mssql-jdbc")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("com.h2database:h2:1.4.200")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("com.azure.spring:spring-cloud-azure-dependencies:${property("springCloudAzureVersion")}")
+	}
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ muer/src/main/kotlin/sch/aut/muer/MuerApplication.kt
@@ -0,0 +1,11 @@
+package sch.aut.muer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MuerApplication
+
+fun main(args: Array<String>) {
+	runApplication<MuerApplication>(*args)
+}
